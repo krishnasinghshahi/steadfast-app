e1bf58fa1780ba2a3d0057cc1fe55e1dc3de40ae
Narendra Ram <narenramm@gmail.com>
2024-11-10 23:45:41 +0530

    don't set broker on server if not available

--- src/api/broker.js
+++ src/api/broker.js
@@ -3,6 +3,12 @@ import axios from 'axios'
 const baseURL = import.meta.env.VITE_BASE_URL
 
 export const updateSelectedBrokerOnServer = async (broker) => {
+  // Return early if no broker or invalid broker object
+  if (!broker || !broker.brokerName || !broker.clientId) {
+    console.log('No valid broker to set')
+    return null
+  }
+
   try {
     const response = await axios.post(`${baseURL}/set-broker`, { broker })
     console.log(response.data.message)
